f76287c7a853cffb82f604972b07de747157bf41
pin0S <pete.g.lynch@gmail.com>
2021-01-28 20:33:34 +1100

    COR origin for netlify site set

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
         auth_token = Knock::AuthToken.new payload: {sub: @user.id}
-        render json: {username: @user.username, jwt: auth_token.token}, status: 200
+        render json: {username: @user.email, jwt: auth_token.token}, status: 200
     else
         render json: {error: "Incorrect Username or Password"}, status: 404 
 
