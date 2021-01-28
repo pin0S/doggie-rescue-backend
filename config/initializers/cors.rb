43e074d1f2dc9ad431033a859fe2c62b618253e9
pin0S <pete.g.lynch@gmail.com>
2021-01-28 20:36:46 +1100

    COR origin for netlify site set

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000',  'https://www.doggierescue.live/'
+    origins 'localhost:3000', '127.0.0.1:3000', 'https://www.doggierescue.live'
 
     resource '*',
       headers: :any,
