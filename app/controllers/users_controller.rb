a74a53c38759dee1bcf40d7d956b6baa08657c37
pin0S <pete.g.lynch@gmail.com>
2021-01-28 20:15:42 +1100

    rmv username from whitelist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.permit(:username, :email, :password, :password_confirmation)
+      params.permit(:email, :password, :password_confirmation)
     end
 end
