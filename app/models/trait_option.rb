db0615793a0d4f738393a7e533b78972dbaaa8a0
ACaspersz <a.casperszz@gmail.com>
2021-01-30 13:37:58 +1100

    adjusted preference models

--- app/models/trait_option.rb
+++ app/models/trait_option.rb
@@ -2,6 +2,7 @@ class TraitOption < ApplicationRecord
     has_many :preferences
     has_many :pet_traits
     belongs_to :trait
-
-    accepts_nested_attributes_for :trait
+    
+    # AC: according to Michaels logic we would also remove the nested attributes here for trait options 
+    # accepts_nested_attributes_for :trait
 end
