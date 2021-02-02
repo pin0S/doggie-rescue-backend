faa29f9e3147f977ccb2930a110092b4ba69e7bb
ACaspersz <a.casperszz@gmail.com>
2021-02-02 17:09:44 +1100

    generated REST client testing, corrected schema,seeds, pref contr

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -46,7 +46,7 @@ class PreferencesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def preference_params
-      params.require(:preferences).permit(:user_id, trait_option_ids: []
-      )
+      params.require(:preference).permit(:user_id, :trait_option_id)
+
     end
 end
